c4ced0a6dbb4c61b8c0178eb4f24ea2d4adb7a7d
Suguru Hirahara <luixxiul@gmail.com>
2017-07-04 21:46:00 +0900

    Add buttonContainer on commonStyles.js
    Setting navigator__buttonContainer_outsideOfURLbar
    to apply border to the bookmark button and publisher button only,
    
    normalize size, placement, draggability etc of
    - navigator__buttonContainer
    - navigator__urlbarForm__buttonContainer_showNoScriptInfo
    - navigator__urlbarForm__urlbarIconContainer
    
    Test Plan: available on https://github.com/brave/browser-laptop/pull/9299#issue-234116287

--- app/renderer/components/navigation/urlBar.js
+++ app/renderer/components/navigation/urlBar.js
@@ -43,6 +43,7 @@ const publisherUtil = require('../../../common/lib/publisherUtil')
 const iconNoScript = require('../../../../img/url-bar-no-script.svg')
 
 const globalStyles = require('../styles/global')
+const commonStyles = require('../styles/commonStyles')
 
 class UrlBar extends React.Component {
   constructor (props) {
@@ -491,7 +492,7 @@ class UrlBar extends React.Component {
       id='urlbar'>
       <div className={cx({
         urlbarIconContainer: true,
-        [css(styles.urlbarForm__urlbarIconContainer)]: true
+        [css(commonStyles.navigator__urlbarForm__urlbarIconContainer)]: true
       })}>
         <UrlBarIcon
           titleMode={this.props.titleMode}
@@ -535,7 +536,7 @@ class UrlBar extends React.Component {
       {
         !this.props.showNoScriptInfo
         ? null
-        : <span className={css(styles.noScriptContainer)}
+        : <span className={css(commonStyles.navigator__urlbarForm__buttonContainer_showNoScriptInfo)}
           onClick={this.onNoScript}>
           <span
             data-l10n-id='noScriptButton'
@@ -588,17 +589,6 @@ const styles = StyleSheet.create({
     borderBottomRightRadius: '0 !important'
   },
 
-  // Create 25x25 square and place .urlbarIcon at the center of it
-  urlbarForm__urlbarIconContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: globalStyles.navigationBar.urlbarForm.height,
-    width: globalStyles.navigationBar.urlbarForm.height,
-    minHeight: globalStyles.navigationBar.urlbarForm.height,
-    minWidth: globalStyles.navigationBar.urlbarForm.height
-  },
-
   urlbarForm__titleBar: {
     display: 'inline-block',
     color: globalStyles.color.chromeText,
@@ -611,13 +601,6 @@ const styles = StyleSheet.create({
 
   urlbarForm__titleBar__host: {
     fontWeight: 600
-  },
-
-  noScriptContainer: {
-    display: 'flex',
-    padding: '5px',
-    marginRight: '-8px',
-    WebkitAppRegion: 'drag'
   },
 
   noScriptButton: {
